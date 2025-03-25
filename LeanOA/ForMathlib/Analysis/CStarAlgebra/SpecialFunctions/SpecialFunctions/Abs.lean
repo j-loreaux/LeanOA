451ee21e675a07b9b44aa882c1b3d49701d093f8
Jon Bannon <jbannon@siena.edu>
2025-03-25 10:50:25 -0400

    Fixed the new `Abs` file

--- LeanOA/ForMathlib/Analysis/CStarAlgebra/SpecialFunctions/SpecialFunctions/Abs.lean
+++ LeanOA/ForMathlib/Analysis/CStarAlgebra/SpecialFunctions/SpecialFunctions/Abs.lean
@@ -4,31 +4,30 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jon Bannon, Jireh Loreaux
 -/
 
-import Mathlib.Analysis.CStarAlgebra.ContinuousFunctionalCalculus.Order
+import LeanOA.ForMathlib.Analysis.SpecialFunctions.ContinuousFunctionalCalculus.Abs
 
 /-!
-# Results for `Abs` that require CStarAlgebra instance
+# Results for `Abs` that require CStarRing instance
 
-This file contains results about Abs that require the full CStarAlgebra machinery.
+This file contains results about Abs that require the full CStarRing machinery.
 -/
-
 section CStar
 
-/- This section should be moved to `ForMathlib/Analysis/CStarAlgebra/SpecialFunctions/Abs.lean`. -/
 
-
-variable [NonUnitalNormedRing A] [StarRing A]
+variable (A : Type*) [NonUnitalNormedRing A] [StarRing A]
 variable [PartialOrder A] [StarOrderedRing A] [NormedSpace ℝ A] [SMulCommClass ℝ A A] [IsScalarTower ℝ A A]
 variable [NonUnitalContinuousFunctionalCalculus ℝ (IsSelfAdjoint : A → Prop)]
 variable [NonnegSpectrumClass ℝ A] [CStarRing A]
 
+open CFC
+
 @[simp]
 lemma abs_eq_zero_iff {a : A}  : abs a = 0 ↔ a = 0 := by
-  rw [abs, sqrt_eq_zero_iff _, CStarRing.star_mul_self_eq_zero_iff]
+  rw [CFC.abs, sqrt_eq_zero_iff _, CStarRing.star_mul_self_eq_zero_iff]
 
 @[simp]
 lemma norm_abs {a : A} : ‖abs a‖ = ‖a‖ := by
   rw [← sq_eq_sq₀ (norm_nonneg _) (norm_nonneg _), sq, sq, ← CStarRing.norm_star_mul_self,
-    abs_nonneg.star_eq, abs_mul_abs_self, CStarRing.norm_star_mul_self]
+    abs_nonneg.star_eq, CFC.abs_mul_abs_self, CStarRing.norm_star_mul_self]
 
 end CStar
