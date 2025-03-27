782b07c025f37cd08e49bef6305b837c5de2f069
Jireh Loreaux <loreaujy@gmail.com>
2025-03-27 13:34:29 -0500

    merge files

--- LeanOA/ForMathlib/Analysis/SpecialFunctions/ContinuousFunctionalCalculus/Abs.lean
+++ LeanOA/ForMathlib/Analysis/SpecialFunctions/ContinuousFunctionalCalculus/Abs.lean
@@ -241,4 +241,26 @@ end Unital
 
 end Generic
 
+section CStar
+
+/- This section requires `A` to be a `CStarRing` -/
+
+variable (A : Type*) [NonUnitalNormedRing A] [StarRing A]
+variable [PartialOrder A] [StarOrderedRing A] [NormedSpace ℝ A] [SMulCommClass ℝ A A] [IsScalarTower ℝ A A]
+variable [NonUnitalContinuousFunctionalCalculus ℝ A IsSelfAdjoint]
+variable [NonnegSpectrumClass ℝ A] [CStarRing A]
+
+open CFC
+
+@[simp]
+lemma abs_eq_zero_iff {a : A}  : abs a = 0 ↔ a = 0 := by
+  rw [CFC.abs, sqrt_eq_zero_iff _, CStarRing.star_mul_self_eq_zero_iff]
+
+@[simp]
+lemma norm_abs {a : A} : ‖abs a‖ = ‖a‖ := by
+  rw [← sq_eq_sq₀ (norm_nonneg _) (norm_nonneg _), sq, sq, ← CStarRing.norm_star_mul_self,
+    abs_nonneg.star_eq, CFC.abs_mul_abs_self, CStarRing.norm_star_mul_self]
+
+end CStar
+
 end CFC
