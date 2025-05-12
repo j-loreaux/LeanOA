d75a497ff8f5ff3b0f32e59a0559c30e8bebbe14
Jireh Loreaux <loreaujy@gmail.com>
2025-05-12 16:05:26 -0500

    feat: continuity of the continuous functional calculus in each variable

--- LeanOA.lean
+++ LeanOA.lean
@@ -1,5 +1,9 @@
 import LeanOA.CFCRange
 import LeanOA.CStarModule.Standard
+import LeanOA.ContinuousFunctionalCalculus.Continuity
+import LeanOA.ContinuousMap.Uniform
+import LeanOA.ForMathlib.Analysis.CStarAlgebra.ContinuousFunctionalCalculus.Order
+import LeanOA.ForMathlib.Analysis.CStarAlgebra.Module.Defs
 import LeanOA.ForMathlib.Analysis.SpecialFunctions.ContinuousFunctionalCalculus.Abs
 import LeanOA.MulNonneg
 import LeanOA.Notation
