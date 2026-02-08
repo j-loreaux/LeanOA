d2b65b7b0dc275b7d7957b0d563ddfcb75ff0dd8
Monica Omar <2497250a@research.gla.ac.uk>
2026-02-08 16:15:55 +0000

    some golfs and stuff

--- LeanOA/CStarAlgebra/FiniteSpectrum.lean
+++ LeanOA/CStarAlgebra/FiniteSpectrum.lean
@@ -1,30 +1,36 @@
 import LeanOA.ForMathlib.Algebra.Star.StarProjection
 import Mathlib.Analysis.CStarAlgebra.ContinuousFunctionalCalculus.Basic
 
+namespace ContinuousMap
+variable {X : Type*} [TopologicalSpace X]
+
 -- move to `Mathlib.Topology.MetricSpace.Pseudo.Defs`?
 /-- `Pi.single` as a continuous map `C(X, ℝ)`. -/
-noncomputable abbrev ContinuousMap.single {X : Type*} [TopologicalSpace X] [DiscreteTopology X]
-    (i : X) : C(X, ℝ) := open Classical in .mk (Pi.single i 1)
+noncomputable abbrev single [DiscreteTopology X] (i : X) : C(X, ℝ) :=
+  open Classical in .mk (Pi.single i 1)
 
-@[simp] theorem ContinuousMap.isStarProjection_single {X : Type*} [TopologicalSpace X]
-    [DiscreteTopology X] (i : X) : IsStarProjection (single i) := by
-  constructor <;> ext <;> simp [Pi.single_apply]
+@[simp] theorem isStarProjection_single [DiscreteTopology X] (i : X) :
+    IsStarProjection (single i) := by constructor <;> ext <;> simp [Pi.single_apply]
 
-@[simp] lemma ContinuousMap.mem_span_isStarProjection_of_finite {X : Type*} [TopologicalSpace X]
-    [T1Space X] [Finite X] (f : C(X, ℝ)) :
-    f ∈ Submodule.span ℝ {p : C(X, ℝ) | IsStarProjection p} := by
+@[simp] lemma mem_span_isStarProjection_of_finite [T1Space X] [Finite X]
+    (f : C(X, ℝ)) : f ∈ Submodule.span ℝ {p : C(X, ℝ) | IsStarProjection p} := by
   have := Fintype.ofFinite X
-  rw [show f = ∑ i, f i • single i by
-    ext x; rw [coe_sum, Finset.sum_apply, Finset.sum_eq_single x] <;> simp_all]
+  rw [show f = ∑ i, f i • single i by ext; simp [Finset.sum_pi_single, ← Pi.single_smul]]
   exact Submodule.sum_mem _ fun i _ ↦ Submodule.smul_mem _ _ <| by simp [Submodule.mem_span_of_mem]
+
+end ContinuousMap
 
 variable {A : Type*} [CStarAlgebra A]
 
 /-- A C⋆-algebra is **FS** if the set of self-adjoint elements has a dense subset of
 elements with finite spectrum. -/
-class CStarAlgebra.FiniteSpectrum (𝕜 A : Type*) [RCLike 𝕜]
-    [TopologicalSpace A] [Ring A] [Algebra 𝕜 A] [StarRing A] : Prop where
+class CStarAlgebra.FiniteSpectrum (𝕜 A : Type*) [RCLike 𝕜] [TopologicalSpace A] [Ring A]
+    [Algebra 𝕜 A] [StarRing A] : Prop where
   fs : {x : A | IsSelfAdjoint x} ⊆ closure {x : A | IsSelfAdjoint x ∧ (spectrum 𝕜 x).Finite}
+
+instance {𝕜 A : Type*} [RCLike 𝕜] [TopologicalSpace A] [Ring A] [Algebra 𝕜 A] [StarRing A]
+    [Subsingleton A] : CStarAlgebra.FiniteSpectrum 𝕜 A where
+  fs := by nontriviality A; exfalso; exact false_of_nontrivial_of_subsingleton A
 
 /-- A self-adjoint element with finite spectrum in a C⋆-algebra is in the span of
 star projections. -/
@@ -44,7 +50,6 @@ theorem CStarAlgebra.FiniteSpectrum.topologicalClosure_span_isStarProjection
     (Submodule.span ℝ {x : A | IsStarProjection x}).topologicalClosure =
       selfAdjoint.submodule ℝ A := by
   refine le_antisymm (fun x hx ↦ closure_minimal (fun x hx ↦ ?_) ?_ hx) fun x hx ↦ ?_
-  · exact Submodule.span_induction (fun _ hx ↦ hx.isSelfAdjoint) (Submodule.zero_mem _)
-      (fun _ _ _ _ hx' hy' ↦ Submodule.add_mem _ hx' hy') (by aesop) hx
+  · refine Submodule.span_induction (fun _ hx ↦ hx.isSelfAdjoint) ?_ ?_ ?_ hx <;> aesop
   · exact isClosed_eq continuous_id'.star continuous_id'
   · exact closure_mono (fun y hy ↦ hy.1.mem_span_isStarProjection_of_finite_spectrum hy.2) (h.fs hx)
