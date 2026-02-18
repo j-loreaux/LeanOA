5d40a7efb410253e6b9b8573668e38aa8ce43a44
Jon Bannon <jbannon@siena.edu>
2026-02-18 17:10:52 -0500

    Unsatisfying initial attempt. Should get this after head clears a bit...

--- LeanOA/Masa.lean
+++ LeanOA/Masa.lean
@@ -1,6 +1,7 @@
 import Mathlib.Analysis.CStarAlgebra.ContinuousFunctionalCalculus.Basic
 
-variable {R A : Type*} [CommSemiring R] [NonUnitalNonAssocCommRing A] [Module R A] [Star A]
+variable {R A : Type*} [CommSemiring R] [NonUnitalSemiring A] [Module R A]
+variable [StarRing A] [StarRing R] [IsScalarTower R A A] [SMulCommClass R A A] [StarModule R A]
 variable (hRA : NonUnitalStarSubalgebra R A)
 
 class NonUnitalStarSubalgebra.IsMasa (B : NonUnitalStarSubalgebra R A) : Prop where
@@ -9,8 +10,35 @@ class NonUnitalStarSubalgebra.IsMasa (B : NonUnitalStarSubalgebra R A) : Prop wh
     ∀ C : NonUnitalStarSubalgebra R A,
       B ≤ C → ∀ (a b : C), a * b = b * a → C ≤ B
 
+/- Need some access lemmas for the above class. -/
+
 /- Let's prove that any commutative `NonUnitalStarSubalgebra` of `A` is contained
 in a Masa of `A` using Zorn. -/
 
-theorem exists_le_masa (B : NonUnitalStarSubalgebra R A) : ∃ (C : NonUnitalStarSubalgebra R A),
-    B ≤ C ∧ C.IsMasa  := by sorry
+theorem exists_le_masa (B : {C : NonUnitalStarSubalgebra R A // (∀ x y : C, x * y = y * x)}) :
+    ∃ (C : NonUnitalStarSubalgebra R A), B ≤ C ∧ C.IsMasa  := by
+  have := by
+    refine zorn_le (α := {C : NonUnitalStarSubalgebra R A // (∀ x y : C, x * y = y * x) ∧ B ≤ C}) ?_
+    intro chain hchain
+    have I : Nonempty {C : NonUnitalStarSubalgebra R A // (∀ x y : C, x * y = y * x) ∧ B ≤ C} := by
+      refine nonempty_subtype.mpr ?_
+      have le_self : B ≤ B := Subtype.coe_le_coe.mp fun ⦃x⦄ a ↦ a
+      use B
+      exact ⟨B.2 , le_self⟩
+    have II : Nonempty chain := by sorry
+    have J := NonUnitalStarSubalgebra.coe_iSup_of_directed (R := R) (A := A)
+      (ι := chain)
+      (S := fun chain ↦ chain.1)
+    have s := iSup (fun (chain : { C : NonUnitalStarSubalgebra R A //
+        (∀ (x y : C), x * y = y * x) ∧ B ≤ C }) ↦ chain.1)
+    have hs1 : ∀ (x y : s), x * y = y * x := by sorry
+    have hs2 : B ≤ s := sorry
+    use ⟨s, hs1, hs2⟩
+    refine mem_upperBounds.mpr ?_
+    intro x hx
+    sorry
+  obtain ⟨M, hM⟩ := this
+  use M
+  constructor
+  · sorry
+  · sorry
