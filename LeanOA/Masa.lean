3cb26a5bb769b836de352a893bc9cf5a24ee3f3c
Jon Bannon <jbannon@siena.edu>
2026-02-19 09:04:15 -0500

    Jireh cleaner version

--- LeanOA/Masa.lean
+++ LeanOA/Masa.lean
@@ -1,26 +1,28 @@
 import Mathlib.Analysis.CStarAlgebra.ContinuousFunctionalCalculus.Basic
 
+section NonUnitalStarSubalgebra
+
 variable {R A : Type*} [CommSemiring R] [NonUnitalRing A] [Module R A]
   [StarRing R] [StarRing A] [IsScalarTower R A A] [SMulCommClass R A A] [StarModule R A]
 
 class NonUnitalStarSubalgebra.IsMasa (B : NonUnitalStarSubalgebra R A) : Prop where
-  comm (a b : B)  : a * b = b * a
+  comm : ∀ a ∈ B, ∀ b ∈ B, a * b = b * a
   maximal (C : NonUnitalStarSubalgebra R A) (hC : ∀ a ∈ C, ∀ b ∈ C, a * b = b * a)
       (hBC : B ≤ C) : C ≤ B
 
-theorem exists_le_masa
-  (B : {C : NonUnitalStarSubalgebra R A // (∀ x ∈ C, ∀ y ∈ C, x * y = y * x)}) :
+theorem exists_le_masa (B : {C : NonUnitalStarSubalgebra R A //
+  (∀ x ∈ C, ∀ y ∈ C, x * y = y * x)}) :
     ∃ (C : NonUnitalStarSubalgebra R A), B ≤ C ∧ C.IsMasa  := by
-  have := by
-    refine zorn_le (α := {C : NonUnitalStarSubalgebra R A //
-      (∀ x ∈ C, ∀ y ∈ C, x * y = y * x) ∧ B ≤ C}) ?_
+  obtain ⟨C, hC⟩ := by
+    apply zorn_le (α := {C : NonUnitalStarSubalgebra R A //
+      (∀ x ∈ C, ∀ y ∈ C, x * y = y * x) ∧ B ≤ C})
     intro chain hchain
     obtain (rfl | h) := chain.eq_empty_or_nonempty
     · exact ⟨⟨B.val, B.prop, le_rfl⟩, by simp⟩
     · have := h.to_subtype
       have hdir : Directed (· ≤ ·) fun S : chain ↦ S.val.val :=
         hchain.directedOn.directed_val.mono_comp _ (by simp)
-      let bound : NonUnitalStarSubalgebra R A := ⨆ S : chain, S.val.val
+      let bound : NonUnitalStarSubalgebra R A := ⨆ S : chain, S
       refine ⟨⟨bound, ?_, ?_⟩, ?_⟩
       · simp only [bound]
         intro a ha b hb
@@ -35,20 +37,7 @@ theorem exists_le_masa
       · intro S hS
         lift S to chain using hS
         exact le_iSup (fun S : chain ↦ S.val.val) _
-  obtain ⟨M, M1⟩ := this
-  use M.1
-  constructor
-  · exact M.2.2
-  · constructor
-    · simpa using M.2.1
-    · dsimp [IsMax] at M1
-      intro C hC hBC
-      simp_all only [Subtype.forall, forall_and_index]
-      obtain ⟨b, hb⟩ := B
-      obtain ⟨m , hm⟩ := M
-      obtain ⟨left, right⟩ := hm
-      simp_all only [Subtype.mk_le_mk, implies_true]
-      simp_all only [implies_true, le_refl]
-      apply M1 C hC
-      · exact NonUnitalStarSubalgebra.toNonUnitalSubalgebra_le_iff.mp fun ⦃x⦄ a ↦ hBC (right a)
-      · exact NonUnitalStarSubalgebra.toNonUnitalSubalgebra_le_iff.mp hBC
+  exact ⟨C, C.prop.2, ⟨C.prop.1, fun S h_comm hCS ↦ @hC ⟨S, h_comm, C.prop.2.trans hCS⟩ hCS⟩⟩
+
+
+end NonUnitalStarSubalgebra
