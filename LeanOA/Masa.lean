7970294d8f04834b0612e0c874b4e737d31d5367
Jon Bannon <jbannon@siena.edu>
2026-02-18 20:12:06 -0500

    Jireh got the zorn part.

--- LeanOA/Masa.lean
+++ LeanOA/Masa.lean
@@ -1,39 +1,42 @@
 import Mathlib.Analysis.CStarAlgebra.ContinuousFunctionalCalculus.Basic
 
-variable {R A : Type*} [CommSemiring R] [NonUnitalSemiring A] [Module R A]
-variable [StarRing A] [StarRing R] [IsScalarTower R A A] [SMulCommClass R A A] [StarModule R A]
-variable (hRA : NonUnitalStarSubalgebra R A)
+variable {R A : Type*} [CommSemiring R] [NonUnitalRing A] [Module R A]
+  [StarRing R] [StarRing A] [IsScalarTower R A A] [SMulCommClass R A A] [StarModule R A]
 
 class NonUnitalStarSubalgebra.IsMasa (B : NonUnitalStarSubalgebra R A) : Prop where
   comm (a b : B)  : a * b = b * a
-  maximal :
-    ∀ C : NonUnitalStarSubalgebra R A,
-      B ≤ C → ∀ (a b : C), a * b = b * a → C ≤ B
+  maximal (C : NonUnitalStarSubalgebra R A) (hC : ∀ a ∈ C, ∀ b ∈ C, a * b = b * a)
+      (hBC : B ≤ C) : C ≤ B
 
-/- Need some access lemmas for the above class. -/
-
-/- Let's prove that any commutative `NonUnitalStarSubalgebra` of `A` is contained
-in a Masa of `A` using Zorn. -/
-
-theorem exists_le_masa (B : {C : NonUnitalStarSubalgebra R A // (∀ x y : C, x * y = y * x)}) :
+theorem exists_le_masa
+  (B : {C : NonUnitalStarSubalgebra R A // (∀ x ∈ C, ∀ y ∈ C, x * y = y * x)}) :
     ∃ (C : NonUnitalStarSubalgebra R A), B ≤ C ∧ C.IsMasa  := by
   have := by
-    refine zorn_le (α := {C : NonUnitalStarSubalgebra R A // (∀ x y : C, x * y = y * x) ∧ B ≤ C}) ?_
+    refine zorn_le (α := {C : NonUnitalStarSubalgebra R A //
+      (∀ x ∈ C, ∀ y ∈ C, x * y = y * x) ∧ B ≤ C}) ?_
     intro chain hchain
-    have I : Nonempty chain := by sorry
-    have J := NonUnitalStarSubalgebra.coe_iSup_of_directed (R := R) (A := A)
-      (ι := chain)
-      (S := fun chain ↦ chain.1)
-    have s := iSup (fun (chain : { C : NonUnitalStarSubalgebra R A //
-        (∀ (x y : C), x * y = y * x) ∧ B ≤ C }) ↦ chain.1)
-    have hs1 : ∀ (x y : s), x * y = y * x := by sorry
-    have hs2 : B ≤ s := sorry
-    use ⟨s, hs1, hs2⟩
-    refine mem_upperBounds.mpr ?_
-    intro x hx
-    sorry
-  obtain ⟨M, hM⟩ := this
-  use M
+    obtain (rfl | h) := chain.eq_empty_or_nonempty
+    · exact ⟨⟨B.val, B.prop, le_rfl⟩, by simp⟩
+    · have := h.to_subtype
+      have hdir : Directed (· ≤ ·) fun S : chain ↦ S.val.val :=
+        hchain.directedOn.directed_val.mono_comp _ (by simp)
+      let bound : NonUnitalStarSubalgebra R A := ⨆ S : chain, S.val.val
+      refine ⟨⟨bound, ?_, ?_⟩, ?_⟩
+      · simp only [bound]
+        intro a ha b hb
+        rw [← SetLike.mem_coe, NonUnitalStarSubalgebra.coe_iSup_of_directed hdir,
+          Set.mem_iUnion] at ha hb
+        obtain ⟨S, hS⟩ := ha
+        obtain ⟨T, hT⟩ := hb
+        obtain ⟨V, hV, hSV, hTV⟩ := hchain.directedOn _ S.prop _ T.prop
+        exact V.prop.1 _ (hSV hS) _ (hTV hT)
+      · exact Classical.arbitrary chain |>.val.prop.2 |>.trans <|
+          le_iSup (fun S : chain ↦ S.val.val) _
+      · intro S hS
+        lift S to chain using hS
+        exact le_iSup (fun S : chain ↦ S.val.val) _
+  obtain ⟨M, M1⟩ := this
+  use M.1
   constructor
   · sorry
   · sorry
