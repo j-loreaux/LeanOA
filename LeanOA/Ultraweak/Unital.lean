6f3bad8f8e6c150e2201ec8b1e0aee42d23bc451
Jon Bannon <jbannon@siena.edu>
2026-02-24 14:03:13 -0500

    Small cleanup

--- LeanOA/Ultraweak/Unital.lean
+++ LeanOA/Ultraweak/Unital.lean
@@ -28,5 +28,4 @@ theorem exists_extremePoint_closedBall [NonUnitalCStarAlgebra M] [NormedAddCommG
 
 noncomputable def CStarAlgofExtreme [NonUnitalCStarAlgebra M] [NormedAddCommGroup P]
   [NormedSpace ℂ P] [Predual ℂ M P] : CStarAlgebra M :=
-    CStarAlgebra.ofExtremePt (A := M)
-      (Classical.choose_spec (exists_extremePoint_closedBall (M := M) (P := P)))
+    CStarAlgebra.ofExtremePt <| Classical.choose_spec <| exists_extremePoint_closedBall (P := P)
