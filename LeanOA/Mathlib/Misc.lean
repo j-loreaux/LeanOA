5609e00263b8a1e05d886b068d0926b2ddf11047
Monica Omar <23701951+themathqueen@users.noreply.github.com>
2026-02-24 15:38:33 +0000

    feat: Sakai 1.6.1 (#64)
    Sakai 1.6.1 says that a C*-algebra is unital iff there exists an extreme point of the closed unit ball.
    
    If it's unital, then 1 is an extreme point of the closed unit ball (already shown).
    So this PR shows the other direction.

--- LeanOA/Mathlib/Misc.lean
+++ LeanOA/Mathlib/Misc.lean
@@ -129,34 +129,6 @@ lemma toEquiv_uniformOfEquivCompactToT2 :
 
 end Continuous
 
-section normedSpaceClosedUnitBall
-variable {𝕜 H : Type*} [RCLike 𝕜] [NormedAddCommGroup H] [NormedSpace 𝕜 H]
-
-open ComplexOrder Set Metric
-
-theorem subsingleton_of_zero_mem_extremePoints_closedUnitBall
-    (h : 0 ∈ extremePoints 𝕜 (closedBall (0 : H) 1)) : Subsingleton H := by
-  by_contra!
-  obtain ⟨y, hy⟩ := exists_ne (0 : H)
-  set z := (1 / ‖y‖ : 𝕜) • y
-  have hz : z ∈ closedBall (0 : H) 1 ∧ ‖z‖ = 1 := by simp [norm_smul, norm_ne_zero_iff.mpr hy, z]
-  simp only [mem_extremePoints, mem_closedBall, dist_zero_right] at h
-  have := h.2 z hz.2.le (-z) (norm_neg z ▸ hz.2.le) ⟨1 / 2, ⟨1 / 2, by simp [-one_div]⟩⟩
-  simp_all
-
-theorem norm_eq_one_of_mem_extremePoints_closedUnitBall [Nontrivial H] {x : H}
-    (hx : x ∈ extremePoints 𝕜 (closedBall (0 : H) 1)) : ‖x‖ = 1 := by
-  have h : x ≠ 0 := fun h ↦
-    have := subsingleton_of_zero_mem_extremePoints_closedUnitBall (h ▸ hx)
-    false_of_nontrivial_of_subsingleton H
-  simp only [mem_extremePoints, mem_closedBall, dist_zero_right] at hx
-  by_contra!
-  refine h (@hx.2 ((1 / ‖x‖ : 𝕜) • x) ?_ 0 (by simp) ⟨‖x‖, 1 - ‖x‖, by simp_all, ?_, ?_⟩).2.symm
-  on_goal 2 => rw [sub_pos, ← RCLike.ofReal_one (K := 𝕜), RCLike.ofReal_lt_ofReal]; grind
-  all_goals simp [norm_smul, norm_ne_zero_iff.mpr h]
-
-end normedSpaceClosedUnitBall
-
 end UniformEquiv
 
 /-! ## Unnecessary
