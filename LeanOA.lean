ee8f2481a8362c626e937195ae817bf47d184d1a
Jireh Loreaux <loreaujy@gmail.com>
2026-02-26 15:57:22 -0600

    mk_all

--- LeanOA.lean
+++ LeanOA.lean
@@ -8,7 +8,9 @@ import LeanOA.ComplexOrder
 import LeanOA.KreinSmulian
 import LeanOA.Lp.Holder
 import LeanOA.Lp.lpSpace
+import LeanOA.Mathlib.Algebra.Order.Module.PositiveLinearMap
 import LeanOA.Mathlib.Algebra.Order.Star.Basic
+import LeanOA.Mathlib.Algebra.Order.Star.Conjugate
 import LeanOA.Mathlib.Algebra.Star.StarAlgHom
 import LeanOA.Mathlib.Algebra.Star.StarProjection
 import LeanOA.Mathlib.Analysis.CStarAlgebra.ApproximateUnit
@@ -26,8 +28,10 @@ import LeanOA.Mathlib.Analysis.Convex.Extreme
 import LeanOA.Mathlib.Analysis.InnerProductSpace.Basic
 import LeanOA.Mathlib.Analysis.Normed.Extreme
 import LeanOA.Mathlib.Analysis.RCLike.Extend
+import LeanOA.Mathlib.Analysis.SpecialFunctions.ContinuousFunctionalCalculus.Rpow.Basic
 import LeanOA.Mathlib.LinearAlgebra.Complex.Module
 import LeanOA.Mathlib.Misc
+import LeanOA.Mathlib.Order.Hom.Basic
 import LeanOA.Mathlib.Topology.ContinuousMap.Bounded.Normed
 import LeanOA.Mathlib.Topology.ContinuousMap.Compact
 import LeanOA.Mathlib.Topology.ContinuousMap.ContinuousMapZero
