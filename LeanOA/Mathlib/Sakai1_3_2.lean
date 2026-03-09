bba4146de6fb26302f9b5912680f854b1dfa1d31
Jon Bannon <jbannon@siena.edu>
2026-03-09 12:37:20 -0400

    Kick off new branch

---
+++ LeanOA/Mathlib/Sakai1_3_2.lean
@@ -0,0 +1,7 @@
+import Mathlib.Analysis.CStarAlgebra.ContinuousMap
+
+variable {K} [TopologicalSpace K] [T2Space K] [CompactSpace K]
+
+open ContinuousFunctions
+
+theorem stonean_continuous_iff : True := by sorry
