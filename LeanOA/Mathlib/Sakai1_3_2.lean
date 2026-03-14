468e161e352f5e2f07269cca06857cf1639556dc
Jon Bannon <jbannon@siena.edu>
2026-03-13 20:55:41 -0400

    This statement might do.

--- LeanOA/Mathlib/Sakai1_3_2.lean
+++ LeanOA/Mathlib/Sakai1_3_2.lean
@@ -9,7 +9,6 @@ open ContinuousFunctions
 /- The following is wrong because it must be true for every x with these properties and
 every I, not just one of them-/
 theorem ExtremallyDisconnected_of_Monotone_ConditionallyCompletePartialOrderSup
-    {I} [PartialOrder I] [IsDirectedOrder I] (x : I → C(K, ℝ))
-    (hfmono : Monotone x) (hcc : ConditionallyCompletePartialOrderSup {x i | i : I})
-    : ExtremallyDisconnected K :=
+    : (∀ I [PartialOrder I] [IsDirectedOrder I], ∀ x : I → C(K, ℝ),
+      Monotone x → ConditionallyCompletePartialOrderSup {x i | i : I}) → ExtremallyDisconnected K :=
   by sorry
