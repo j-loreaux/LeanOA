0ddba42c9f681274efd126462feb56153eea16d5
Jon Bannon <jbannon@siena.edu>
2026-03-13 20:56:09 -0400

    Maybe...

--- LeanOA/Mathlib/Sakai1_3_2.lean
+++ LeanOA/Mathlib/Sakai1_3_2.lean
@@ -6,8 +6,6 @@ variable {K} [TopologicalSpace K] [T2Space K] [CompactSpace K]
 
 open ContinuousFunctions
 
-/- The following is wrong because it must be true for every x with these properties and
-every I, not just one of them-/
 theorem ExtremallyDisconnected_of_Monotone_ConditionallyCompletePartialOrderSup
     : (∀ I [PartialOrder I] [IsDirectedOrder I], ∀ x : I → C(K, ℝ),
       Monotone x → ConditionallyCompletePartialOrderSup {x i | i : I}) → ExtremallyDisconnected K :=
