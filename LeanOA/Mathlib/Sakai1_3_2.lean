90f357c248e08392f180ba88bcc411330a76268e
Jon Bannon <jbannon@siena.edu>
2026-03-13 20:42:09 -0400

    Test statement

--- LeanOA/Mathlib/Sakai1_3_2.lean
+++ LeanOA/Mathlib/Sakai1_3_2.lean
@@ -1,7 +1,13 @@
 import Mathlib.Analysis.CStarAlgebra.ContinuousMap
+import Mathlib.Topology.ContinuousMap.Ordered
+import Mathlib.Topology.Category.Stonean.Basic
 
 variable {K} [TopologicalSpace K] [T2Space K] [CompactSpace K]
 
 open ContinuousFunctions
 
-theorem stonean_continuous_iff : True := by sorry
+theorem ExtremallyDisconnected_of_Blah {I} [PartialOrder I] [IsDirectedOrder I] (x : I → C(K, ℝ))
+    (hfmono : Monotone x)
+    (hsup_of_bdd : Set.Bounded (α := C(K, ℝ)) LE.le {x i | i : I} → SupSet {x i | i : I})
+    : ExtremallyDisconnected K :=
+  by sorry
