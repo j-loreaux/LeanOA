dfc010c4ab6ea59e6e7958d1d03dac5c86bc3252
Jon Bannon <jbannon@siena.edu>
2026-03-14 09:24:40 -0400

    Budged

--- LeanOA/Mathlib/Sakai1_3_2.lean
+++ LeanOA/Mathlib/Sakai1_3_2.lean
@@ -6,7 +6,21 @@ variable {K} [TopologicalSpace K] [T2Space K] [CompactSpace K]
 
 open ContinuousFunctions NNReal
 
-theorem ExtremallyDisconnected_of_ConditionallyCompletePartialOrderSup
-    : ∀ s : Set C(K, ℝ≥0), DirectedOn (· ≤ ·) s → s.Nonempty → BddAbove s →
-    ExtremallyDisconnected K :=
-  by sorry
+theorem ExtremallyDisconnected_of_notSureWhatYet
+    : (∀ s : Set C(K, ℝ≥0), DirectedOn (· ≤ ·) s → s.Nonempty → BddAbove s →
+    ∃ (f : C(K, ℝ≥0)), IsLUB s f) → ExtremallyDisconnected K :=
+  by
+  intro H1
+  constructor
+  intro U hU
+  by_contra H2
+  have hne : Nonempty U := by
+    by_contra
+    push_neg at this
+    simp only [Set.isEmpty_coe_sort] at this
+    rw [this, closure_empty] at H2
+    simp only [isOpen_empty, not_true_eq_false] at H2
+  obtain ⟨t, ht⟩ := hne
+  have hR : DirectedOn (· ≤ ·) {f : C(K, ℝ≥0) | ∀ x, 0 ≤ f x ∧ f x ≤ 1} := by sorry
+  have hT := H1 {f : C(K, ℝ≥0) | ∀ x, 0 ≤ f x ∧ f x ≤ 1} hR
+  sorry
