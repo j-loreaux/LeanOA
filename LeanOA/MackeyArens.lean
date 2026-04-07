b92be2670e57c743d0cf2e4d0afeef474d96c0ed
Jon Bannon <jbannon@siena.edu>
2026-04-07 19:52:56 -0400

    Removing some redundancies

--- LeanOA/MackeyArens.lean
+++ LeanOA/MackeyArens.lean
@@ -10,21 +10,16 @@ open Set Topology WeakDual Metric
 variable {𝕜 V F : Type*} [NontriviallyNormedField 𝕜] [AddCommGroup V]
     [Module 𝕜 V] [AddCommGroup F] [Module 𝕜 F]
 
-def seminorm_point_on_F (v : V) (b : F →ₗ[𝕜] V →ₗ[𝕜] 𝕜) : Seminorm 𝕜 F :=
-    Seminorm.comp (normSeminorm 𝕜 𝕜) (b.flip v)
-
-def seminorm_point_on_V (f : F) (b : F →ₗ[𝕜] V →ₗ[𝕜] 𝕜) : Seminorm 𝕜 V :=
-    Seminorm.comp (normSeminorm 𝕜 𝕜) (b f)
-
-/- I think this is already in Mathlib as `polar` associated to `b`. -/
+/- I think this is already in Mathlib as `polar` associated to `b`.
+Also, this has nothing to do with neighborhoods. -/
 def nhd_polar (U : Set V) (b : F →ₗ[𝕜] V →ₗ[𝕜] 𝕜) : Set F :=
-  {f | ∀ v ∈ U, (seminorm_point_on_F v b) f ≤ 1}
+  {f | ∀ v ∈ U, Seminorm.comp (normSeminorm 𝕜 𝕜) (b.flip v) f ≤ 1}
 
 variable [TopologicalSpace V] [ContinuousSMul 𝕜 V]
 
 variable (U : Set V) (b : F →ₗ[𝕜] V →ₗ[𝕜] 𝕜) (U_nhds : U ∈ 𝓝 (0 : V))
 
-def s := {p | ∃ f ∈ nhd_polar U b, p = seminorm_point_on_V f b}
+def s := {p | ∃ f ∈ nhd_polar U b, p = Seminorm.comp (normSeminorm 𝕜 𝕜) (b f)}
 
 lemma absorbing (v : V) : ∃ t : 𝕜, t ≠ 0 ∧ t • v ∈ U := by sorry
 
@@ -36,13 +31,11 @@ lemma pointwise_bound (v : V) : ∃ M, ∀ p ∈ s U b, p v ≤ M := by
   intro p hp
   obtain ⟨f, hfmem, hdef⟩ := hp
   rw [hdef]
-  dsimp [seminorm_point_on_V]
   have A := LinearMap.polar_mem b (fun x ↦ ‖(b x) (t • v)‖ ≤ 1)
      (t • v) (fun x a ↦ a) f (hfmem (t • v) ht)
   grw [LinearMap.map_smul (b f) t v, norm_smul t ((b f) v)] at A
   have F : ‖t‖ * ‖t‖⁻¹ = 1 := by aesop
-  have D : ‖t‖ * ‖(b f) v‖ ≤ ‖t‖ * ‖t‖⁻¹ := le_of_le_of_eq A (id (Eq.symm F))
-  exact (mul_le_mul_iff_of_pos_left h).mp D
+  exact (mul_le_mul_iff_of_pos_left h).mp <| le_of_le_of_eq A (id (Eq.symm F))
 
 theorem correct_bddness : BddAbove ((↑)'' (s U b) : Set (V → ℝ)) := by
   choose g hg using pointwise_bound U b
